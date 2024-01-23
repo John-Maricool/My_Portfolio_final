88e565481e404ad966151f83c14a11325a934025
John-Maricool <ebukaikenwa0902@gmail.com>
2024-01-23 00:15:16 -1100

    Some changes done

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -3,13 +3,6 @@ import { useState } from "react";
 import { myData3 } from "../data";
 import Modal from "./Modal";
 
-interface Article {
-  id: number;
-  title: string;
-  image: string;
-  para: string;
-}
-
 const Blog = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
